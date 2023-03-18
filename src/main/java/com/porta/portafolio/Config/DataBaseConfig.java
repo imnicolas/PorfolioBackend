2a4443e5a7781d534b82ce2c5715282025070500
imnicolas <nicolas.esteban.suarez@hotmail.es>
2023-03-18 15:00:55 -0300

    check another improve for variables env.

---
+++ src/main/java/com/porta/portafolio/Config/DataBaseConfig.java
@@ -0,0 +1,23 @@
+//package com.porta.portafolio.Config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class DataBaseConfig {
+//
+//    @Value("${DB_USERNAME}")
+//    private String dbUsername;
+//
+//    @Value("${DB_PASSWORD}")
+//    private String dbPassword;
+//
+//    @Value("${DB_HOST}")
+//    private String dbHost;
+//
+//    @Value("${DB_PORT}")
+//    private String dbPort;
+//
+//    @Value("${DB_NAME}")
+//    private String dbName;
+//}
